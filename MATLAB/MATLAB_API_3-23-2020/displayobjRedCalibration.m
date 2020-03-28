f05df427277cae4036e3bd2c53b8fd4edc366aa1
JaySpock <61716139+JaySpock@users.noreply.github.com>
2020-03-28 16:42:01 -0600

    Updated MATLAB API 3/28/2020
    Changed to a more robust manual method in red to test

--- MATLAB/MATLAB_API_3-23-2020/displayobjRedCalibration.m
+++ MATLAB/MATLAB_API_3-23-2020/displayobjRedCalibration.m
@@ -1,79 +1,83 @@
 function displayobjRedCalibration(inbytes,handles)
 % Function that displays the sensor's image. First grabs the values from
 % the sensor and reshapes them in a 3 dimensions vector. 
-global keeprunred fullimage lock RedOn RedOff RedZero next Red0 Red1 Red2 Red3 Red0_img Red1_img Red2_img Red3_img
+global keeprunred fullimage lock RedOn RedOff RedZero next Red1 Red2 Red3 Red4 Red1_img Red2_img Red3_img Red4_img
 
 %img=uint8(inbytes.GetImageData.GetRawPixels1Byte);
 keeprunred = true;
 while keeprunred
 switch next
-    case 'Zeroth'
-        Red0 = double(inbytes.GetImageData.GetRawPixels1Byte);
-        Red0_img = reshape(Red0(), [250,250]);
-        pause(0.005);
-        next = 'First';
-        keeprunred = false;
     case 'First'
         Red1 = double(inbytes.GetImageData.GetRawPixels1Byte);
         Red1_img = reshape(Red1(), [250,250]);
-        pause(0.005);
-        next = 'Second';
+        pause(0.006);
+        next = 'First';
         keeprunred = false;
     case 'Second'
         Red2 = double(inbytes.GetImageData.GetRawPixels1Byte);
         Red2_img = reshape(Red2(), [250,250]);
-        pause(0.005);
-        next = 'Third';
+        pause(0.006);
+        next = 'Second';
         keeprunred = false;
     case 'Third'
         Red3 = double(inbytes.GetImageData.GetRawPixels1Byte);
         Red3_img = reshape(Red3(), [250,250]);
+        pause(0.006);
+        next = 'Third';
+        keeprunred = false;
+    case 'Fourth'
+        Red4 = double(inbytes.GetImageData.GetRawPixels1Byte);
+        Red4_img = reshape(Red4(), [250,250]);
 %         save('Red1_2mm.mat','Red1');
 %         save('Red2_2mm.mat','Red2');
 %         save('Red3_2mm.mat','Red3');
         tiledlayout(1,4)
         nexttile
-        image(Red0_img);
+        image(Red1_img); title('1');
         nexttile
-        image(Red1_img);
+        image(Red2_img); title('2');
         nexttile
-        image(Red2_img);
+        image(Red3_img); title('3');
         nexttile
-        image(Red3_img);
+        image(Red4_img); title('4');
         
-        order1 = input('Enter (1) if the first image is red turning on, (2) if red turning off, or (3) if completely off:  ');
-        order2 = input('Enter (1) if the second image is red turning on, (2) if red turning off, or (3) if completely off:  ');
-        
-        if order1 == 1
-            RedOn = Red1;
-                if order2 == 2
-                    RedOff = Red2;
-                    RedZero = Red3;
-                elseif order2 == 3
-                    RedZero = Red2;
-                    RedOff = Red3;
-                end
-        elseif order1 == 2
-            RedOff = Red1;
-                if order2 == 1
-                    RedOn = Red2;
-                    RedZero = Red3;
-                elseif order2 == 3
-                    RedZero = Red2;
-                    RedOn = Red3;
-                end
-        elseif order1 == 3
-            RedZero = Red1;
-                if order2 == 1
-                    RedOn = Red2;
-                    RedOff = Red3;
-                elseif order2 == 2
-                    RedOff = Red2;
-                    RedOn = Red3;
-                end   
+        oninput = input('Which figure represents red turning on?  ');
+        switch oninput
+            case 1
+                RedOn = Red1;
+            case 2
+                RedOn = Red2;
+            case 3
+                RedOn = Red3;
+            case 4
+                RedOn = Red4;
         end
+        offinput = input('Which figure represents red turning off?  ');
+        switch offinput
+            case 1
+                RedOff = Red1;
+            case 2
+                RedOff = Red2;
+            case 3
+                RedOff = Red3;
+            case 4
+                RedOff = Red4;
+        end
+        zeroinput = input('Which figure represents red completely off?  ');
+        switch zeroinput
+            case 1
+                RedZero = Red1;
+            case 2
+                RedZero = Red2;
+            case 3
+                RedZero = Red3;
+            case 4
+                RedZero = Red4;
+        end
+        
         next = 'Done';
         keeprunred = false;
+        
     case 'Done'
 %         img = uint8(inbytes.GetImageData.GetRawPixels1Byte);
 %         imgh = reshape(img(), [250,250]);
